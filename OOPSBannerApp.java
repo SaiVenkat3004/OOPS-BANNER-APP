7d97cc3f2445f88d21dbd33166f8899b06133d0c
SaiVenkat3004 <sy1704@srmist.edu.in>
2026-02-16 19:53:39 +0530

    UC2 - Banner OOPS implemented

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * UC2 - Print OOPS as Banner
+ * @author Sai
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String space = "   ";
+
+        System.out.println("  *****   " + space + "  *****   " + space + "  ******  " + space + "  *****   ");
+        System.out.println(" *     *  " + space + " *     *  " + space + " *     *  " + space + " *     *  ");
+        System.out.println("*       * " + space + "*       * " + space + "*     *    " + space + "*          ");
+        System.out.println("*       * " + space + "*       * " + space + " ******     " + space + "  *****    ");
+        System.out.println("*       * " + space + "*       * " + space + "*            " + space + "       *   ");
+        System.out.println(" *     *  " + space + " *     *  " + space + "*            " + space + " *     *   ");
+        System.out.println("  *****   " + space + "  *****   " + space + "*            " + space + "  *****    ");
+    }
+}
