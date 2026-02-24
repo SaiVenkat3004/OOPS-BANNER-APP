97ae68822aa6e7f0b0b9254c8742670242cedc9b
SaiVenkat3004 <sy1704@srmist.edu.in>
2026-02-24 11:18:30 +0530

    UC6 - Refactored banner logic into separate method

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,57 +2,65 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("OOPS Banner App - UC5: Inline Array Initialization\n");
+        System.out.println("OOPS Banner App - UC6: Using Methods\n");
 
-        // Declare and initialize array in a single statement
-        String[] banner = {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-                String.join(" ",
-                        "  *****  ",
-                        "  *****  ",
-                        " ******* ",
-                        " ******* "),
+        // Final banner array (7 lines)
+        String[] banner = new String[7];
 
-                String.join(" ",
-                        " **   ** ",
-                        " **   ** ",
-                        " **   ** ",
-                        " **      "),
-
-                String.join(" ",
-                        "**     **",
-                        "**     **",
-                        "**     **",
-                        " **      "),
-
-                String.join(" ",
-                        "**     **",
-                        "**     **",
-                        " ******* ",
-                        "  *****  "),
-
-                String.join(" ",
-                        "**     **",
-                        "**     **",
-                        " **      ",
-                        "      ** "),
-
-                String.join(" ",
-                        " **   ** ",
-                        " **   ** ",
-                        " **      ",
-                        " **   ** "),
-
-                String.join(" ",
-                        "  *****  ",
-                        "  *****  ",
-                        " **      ",
-                        "  *****  ")
-        };
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ",
+                    oPattern[i],
+                    oPattern[i],
+                    pPattern[i],
+                    sPattern[i]);
+        }
 
         // Print using enhanced for loop
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Method for Letter O
+    public static String[] getOPattern() {
+        return new String[] {
+                "  *****  ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "  *****  "
+        };
+    }
+
+    // Method for Letter P
+    public static String[] getPPattern() {
+        return new String[] {
+                " ******* ",
+                " **   ** ",
+                " **   ** ",
+                " ******* ",
+                " **      ",
+                " **      ",
+                " **      "
+        };
+    }
+
+    // Method for Letter S
+    public static String[] getSPattern() {
+        return new String[] {
+                " ******* ",
+                " **      ",
+                " **      ",
+                "  *****  ",
+                "      ** ",
+                "      ** ",
+                " ******* "
+        };
     }
 }
